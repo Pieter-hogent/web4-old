87e5453e4674ad205da4d2e9fd4eba40cf5c220f
Pieter Van Der Helst <pieter@heyheyapps.com>
2018-01-02 16:44:09 +0100

    start of recipe app, with the first component added

---
+++ recipeapp/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import { RecipeComponent } from './recipe/recipe.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    RecipeComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ recipeapp/src/app/recipe/recipe.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-recipe',
+  templateUrl: './recipe.component.html',
+  styleUrls: ['./recipe.component.css']
+})
+export class RecipeComponent implements OnInit {
+  name: string;
+  ingredients: string[];
+
+  constructor() {
+    this.name = 'spaghetti';
+    this.ingredients = ['tomato', 'minced meat', 'onion', 'celery', 'carrot'];
+  }
+
+  ngOnInit() {}
+}
