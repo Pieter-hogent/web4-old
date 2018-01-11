2621152162ddc9bfc03dafeb0089814e02d8d899
Pieter Van Der Helst <pieter@heyheyapps.com>
2018-01-11 11:22:43 +0100

    now with all getters

--- recipeapp/src/app/recipe/recipe.model.ts
+++ recipeapp/src/app/recipe/recipe.model.ts
@@ -2,7 +2,6 @@ export class Recipe {
   private _name: string;
   private _dateAdded: Date = new Date();
   private _ingredients = new Array<string>();
-  private _directions = new Array<string>();
 
   constructor(name: string) {
     this._name = name;
@@ -12,6 +11,9 @@ export class Recipe {
   }
   get dateAdded(): Date {
     return this._dateAdded;
+  }
+  get ingredients(): string[] {
+    return this._ingredients;
   }
   addIngredient(name: string, amount?: number, unit?: string) {
     this._ingredients.push(`${amount || 1} ${unit || ''} ${name}`);
