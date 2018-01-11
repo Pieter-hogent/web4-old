b395818232fd74fdf953c52747f3204d0977269c
Pieter Van Der Helst <pieter@heyheyapps.com>
2018-01-11 12:04:25 +0100

    update recipe component to use an @input for its recipe

--- recipeapp/src/app/app.component.ts
+++ recipeapp/src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { Recipe } from './recipe/recipe.model';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  private _recipes = new Array<Recipe>();
+
+  constructor() {
+    const recipe1 = new Recipe('spaghetti');
+    recipe1.addIngredient('minced meat', 500, 'grams');
+    recipe1.addIngredient('tomato', 0.75, 'liter');
+    recipe1.addIngredient('onion', 2);
+
+    const recipe2 = new Recipe('risotto');
+    recipe2.addIngredient('rice', 200, 'grams');
+    recipe2.addIngredient('parmesan', 50, 'grams');
+
+    this._recipes.push(recipe1);
+    this._recipes.push(recipe2);
+  }
+
+  get recipes() {
+    return this._recipes;
+  }
 }
--- recipeapp/src/app/recipe/recipe.component.ts
+++ recipeapp/src/app/recipe/recipe.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { Recipe } from './recipe.model';
 
 @Component({
@@ -7,18 +7,9 @@ import { Recipe } from './recipe.model';
   styleUrls: ['./recipe.component.css']
 })
 export class RecipeComponent implements OnInit {
-  private _recipe: Recipe;
+  @Input() public recipe: Recipe;
 
-  constructor() {
-    this._recipe = new Recipe('spaghetti');
-    this._recipe.addIngredient('tomato', 0.75, 'liter');
-    this._recipe.addIngredient('onion', 2);
-    this._recipe.addIngredient('minced meat', 500, 'grams');
-  }
-
-  get recipe(): Recipe {
-    return this._recipe;
-  }
+  constructor() {}
 
   ngOnInit() {}
 }
