c44a0cac4751454a56a4a54d3b525fdb93662216
Pieter Van Der Helst <pieter@heyheyapps.com>
2018-01-18 17:11:43 +0100

    use nodemon

--- recipeapp/backend/routes/index.js
+++ recipeapp/backend/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send('server works');
+  res.send('server works with nodemon');
 });
 
 module.exports = router;
