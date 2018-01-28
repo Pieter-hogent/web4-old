50b455bf7f6ec574bf14d30db6542a0cff3e6453
Pieter Van Der Helst <pieter.vanderhelst@hogent.be>
2018-01-28 10:12:39 +0100

    add new recipe converted to basic form

--- recipeapp/frontend/src/app/add-recipe/add-recipe.component.ts
+++ recipeapp/frontend/src/app/add-recipe/add-recipe.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Recipe } from '../recipe/recipe.model';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-add-recipe',
@@ -8,13 +9,16 @@ import { Recipe } from '../recipe/recipe.model';
 })
 export class AddRecipeComponent implements OnInit {
   @Output() public newRecipe = new EventEmitter<Recipe>();
+  private recipe: FormGroup;
+
   constructor() {}
 
-  ngOnInit() {}
-
-  addRecipe(newRecipeName: HTMLInputElement): boolean {
-    const recipe = new Recipe(newRecipeName.value);
-    this.newRecipe.emit(recipe);
-    return false;
+  ngOnInit() {
+    this.recipe = new FormGroup({
+      name: new FormControl('risotto')
+    });
+  }
+  onSubmit() {
+    this.newRecipe.emit(new Recipe(this.recipe.value.name));
   }
 }
--- recipeapp/frontend/src/app/app.module.ts
+++ recipeapp/frontend/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { RecipeComponent } from './recipe/recipe.component';
@@ -16,7 +17,7 @@ import { RecipeFilterPipe } from './recipe-filter.pipe';
     AddRecipeComponent,
     RecipeFilterPipe
   ],
-  imports: [BrowserModule, HttpClientModule],
+  imports: [BrowserModule, HttpClientModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
